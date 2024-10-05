9a83f903347bb707f7b07308edc7a102267fd4e1
tkhs <tokuhausu@gmail.com>
2024-10-06 00:33:50 +0900

    バインド時に初期値をセッターに渡すように変更

--- src/main/java/lifegame/util/Binding.java
+++ src/main/java/lifegame/util/Binding.java
@@ -23,5 +23,6 @@ public class Binding {
 
     public static <T> void bindSetter(State<T> state, Consumer<T> setter) {
         state.onValueChange(setter::accept);
+        setter.accept(state.getValue());
     }
 }
