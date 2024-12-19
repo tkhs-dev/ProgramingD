2b164b292f84313ca61523a4685d4d8d8c7e3716
tkhs <tokuhausu@gmail.com>
2024-12-19 17:47:48 +0900

    データ不整合対策を強化

--- src/main/java/lifegame/model/GameModel.java
+++ src/main/java/lifegame/model/GameModel.java
@@ -3,27 +3,29 @@ package lifegame.model;
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class GameModel {
-    private BoardState boardState;
-    private final ArrayDeque<BoardState> historyStack = new ArrayDeque<>();
+    private AtomicReference<BoardState> boardState;
+    private final Deque<BoardState> historyStack = new ArrayDeque<>();
 
     public GameModel() {
-        this.boardState = new BoardState();
+        this.boardState = new AtomicReference<>(new BoardState());
     }
 
     public BoardState getBoardState() {
-        return boardState;
+        return boardState.get();
     }
 
     public synchronized void step(){
         addHistory();
-        boardState.nextState();
+        boardState.get().nextState();
     }
 
     public synchronized void clear(){
         addHistory();
-        this.boardState = new BoardState();
+        this.boardState.set(new BoardState());
     }
 
     public synchronized void changeCellState(int x, int y, boolean state){
@@ -32,11 +34,11 @@ public class GameModel {
 
     public synchronized void changeCellState(int x, int y, boolean state, boolean addHistory){
         if(addHistory) addHistory();
-        boardState.changeCellState(x, y, state);
+        boardState.get().changeCellState(x, y, state);
     }
 
     private synchronized void addHistory(){
-        historyStack.push(boardState.clone());
+        historyStack.push(boardState.get().clone());
         int HISTORY_SIZE = 32;
         if(historyStack.size() > HISTORY_SIZE){
             historyStack.pollLast();
@@ -47,7 +49,7 @@ public class GameModel {
         if(historyStack.isEmpty()){
             return;
         }
-        boardState = historyStack.pop();
+        boardState.set(historyStack.pop());
     }
 
     public boolean isUndoEnabled(){
@@ -59,7 +61,7 @@ public class GameModel {
         try {
             fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            boardState.writeExternal(objectOutputStream);
+            boardState.get().writeExternal(objectOutputStream);
             objectOutputStream.flush();
             objectOutputStream.close();
         } catch (Exception e) {
@@ -72,7 +74,7 @@ public class GameModel {
         try {
             fileInputStream = new FileInputStream(file);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            boardState.readExternal(objectInputStream);
+            boardState.get().readExternal(objectInputStream);
             objectInputStream.close();
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(null, "File not found");
